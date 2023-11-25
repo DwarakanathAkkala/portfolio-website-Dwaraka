dcc65d2b72411a2d40cda636790a02eaec40ea9d
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2023-11-26 04:52:22 +0530

    Header and Navigation Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,34 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+// Menu Show Y-Axis Hidden
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
+// Menu Show
+// Validate if navToggle constant element exists
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        console.log("Clicked")
+        navMenu.classList.add('show-menu');
+    });
+}
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+// Menu Hidden
+// Validate if navClose constant exists
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
+// Remove Menu Mobile
+const navLink = document.querySelectorAll('.nav_link');
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
-
-/*==================== REMOVE MENU MOBILE ====================*/
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    // When user clicks on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
 /*==================== ACCORDION SKILLS ====================*/
@@ -30,10 +49,10 @@
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
