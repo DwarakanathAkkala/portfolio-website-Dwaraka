c3dc9054bc138fce7d3d47492cb11d2951314b09
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2023-11-27 08:06:38 +0530

    Qualifications Tab Toggle
    Logic Similar to Bootstrap Tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,27 @@ skillsHeader.forEach((ele) => {
 });
 
 
-/*==================== QUALIFICATION TABS ====================*/
+// Qualification Tabs
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active');
+        });
+        target.classList.add('qualification_active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active');
+        });
+        tab.classList.add('qualification_active');
+
+    });
+
+});
 
 
 /*==================== SERVICES MODAL ====================*/
