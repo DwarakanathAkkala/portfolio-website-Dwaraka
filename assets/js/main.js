a1b4239d8d43b31856351d85c33093b3006b17cb
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2023-11-30 10:57:52 +0530

    Form Submit Refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -220,36 +220,19 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected_icon', getCurrIcon());
 });
 
-function sendMessage() {
-
-    if (checkInputs()) {
-        console.log("Valid Data")
-        let params = {
-            userName: document.getElementById('userName').value,
-            userEmail: document.getElementById('userEmail').value,
-            project: document.getElementById('project').value,
-            message: document.getElementById('message').value
-        };
-
-
-        emailjs.send('service_yh60t0i', 'template_5gnagvj', params).then((res) => {
-            if (res = 200) {
-                alert("Hey, Dwaraka Bot here. Message Sent to Dwaraka :-)");
-            }
-            else alert("Hey, Something went wrong. Please try contacting Dwaraka via Phone or email");
-        });
+document.getElementById('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const params = {
+        userName: document.getElementById("userName").value,
+        userEmail: document.getElementById("userEmail").value,
+        project: document.getElementById("project").value,
+        message: document.getElementById("message").value
     }
-}
 
-function checkInputs() {
-    const input = document.querySelectorAll('input');
-
-    for (let i = 0; i < input.length; i++) {
-        input[i].addEventListener('invalid', (e) => {
-            return false;
-        });
-    }
-}
-
-
-
+    emailjs.send('service_yh60t0i', 'template_5gnagvj', params).then(function () {
+        alert("Successfully sent the data. Dwaraka will contact you shortly.");
+        form.reset();
+    }, function (error) {
+        alert("Error", error, " Please contact Dwaraka through Phone or email. Sorry for the inconvenience.");
+    });
+})
